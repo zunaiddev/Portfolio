deb9b657974fa23b22090ee74b88b30b2594495c
Zunaid <ja7667924@gmail.com>
2025-01-26 20:59:27 +0530

    feat: add new project section with achievements and skills to the homepage

---
+++ main.js
@@ -0,0 +1,6 @@
+// let i = 1;
+// const interval = setInterval(() => {
+// 	achievement1.textContent = `+${i}`;
+// 	if (i >= 12) clearInterval(interval); // Stop the interval when done
+// 	i++;
+// }, 100); // Delay of 100ms
