824611ca90c46dc9f8d9209aaa2a6af737346b25
Zunaid <ja7667924@gmail.com>
2025-02-07 19:29:02 +0530

    fix: remove unnecessary comment in main.js for cleaner code

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			});
 		},
 		{ threshold: 0.2 }
-	); // 50% of the element must be visible
+	);
 
 	document
 		.querySelectorAll(".animated-component")
