1e0ea7684bbce19ea15e345566f8bcf4e854f05c
Zunaid <ja7667924@gmail.com>
2025-02-10 20:51:20 +0530

    improved responsiveness and also add some custom validation rules in main.js

--- main.js
+++ main.js
@@ -1,26 +1,77 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const observer = new IntersectionObserver(
-		(entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					entry.target.classList.add("show");
-				}
-			});
-		},
-		{ threshold: 0.2 }
-	);
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                }
+            });
+        },
+        {threshold: 0.2}
+    );
 
-	document
-		.querySelectorAll(".animated-component")
-		.forEach((el) => observer.observe(el));
+    document
+        .querySelectorAll(".animated-component")
+        .forEach((el) => observer.observe(el));
 });
 
-let $form = document.querySelector("#form");
+let form = document.querySelector("#form");
 
-$form.addEventListener("submit", function (e) {
-	e.preventDefault();
-	window.alert(
-		"I apologize, but this functionality is currently not working. Please contact me via email."
-	);
-	$form.reset();
+const validationMessages = {
+    name: {
+        valueMissing: 'Please enter your name.',
+        tooShort: 'name must be at least 3 characters.',
+    },
+    email: {
+        valueMissing: 'Please enter an email address.',
+        typeMismatch: 'Please enter a valid email.',
+    },
+    message: {
+        valueMissing: 'Please enter a message.',
+        tooShort: 'message must be at least 10 characters.',
+    }
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(validateName())
+    validateEmail();
+    form.reset();
 });
+
+// function validateForm() {
+//
+//     let emailValue = email.value.trim();
+//     let messageValue = message.value.trim();
+//
+//     return true;
+// }
+
+function validateName() {
+    console.log("validateName()");
+    let nameValue = document.getElementById("name").value.trim();
+    let nameError = document.querySelector("#nameError");
+
+    if (nameValue !== "" || nameValue.length <= 2) {
+        nameError.innerText = "Please enter a valid name.";
+        nameError.style.display = "block";
+        return false;
+    }
+
+    nameError.style.display = "none";
+    return true;
+}
+
+function validateEmail() {
+    let email = document.getElementById("email");
+    let emailError = document.querySelector("#emailError");
+    if (!email.validity.valid) {
+        emailError.style.display = "block";
+        email.setCustomValidity("Invalid email format!");
+        return false;
+    } else {
+        emailError.style.display = "none";
+        email.setCustomValidity("");
+        return true;
+    }
+}
